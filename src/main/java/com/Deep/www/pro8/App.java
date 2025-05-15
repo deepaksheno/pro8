e13f22b117dd83ff427ddfd6916753243bbb7bd3
deepaksheno <deepakshenoy119@gmail.com>
2025-05-15 09:40:14 +0530

    first commit

---
+++ src/main/java/com/Deep/www/pro8/App.java
@@ -0,0 +1,13 @@
+package com.Deep.www.pro8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
